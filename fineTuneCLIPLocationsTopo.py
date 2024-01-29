ab092c85866db7582808bc13b79633c095845a0d
claudaff <145538566+claudaff@users.noreply.github.com>
2024-01-29 15:39:11 +0100

    Update and rename fineTuneCLIPTopArea.py to fineTuneCLIPLocationsTopo.py

--- fineTuneCLIPLocationsTopo.py
+++ fineTuneCLIPLocationsTopo.py
@@ -13,7 +13,7 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-# Python script to fine-tune CLIP for topographic areas
+# Python script to fine-tune CLIP for topographic locations
 # Adopted and adapted from: https://github.com/statscol/clip-fine-tuning/blob/main/clip-fine-tuning.ipynb
 
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
